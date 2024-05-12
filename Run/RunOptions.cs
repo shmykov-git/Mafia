54f504779b72626a155b375d673a6e1500472104
AlexeyShmykov <shmykov.dev@gmail.com>
2024-05-12 20:07:57 +0400

    delete run

--- Run/RunOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mafia.Model;
-
-public class RunOptions
-{
-    public bool HostInstructions { get; set; }
-    public bool CitySelections { get; set; }
-    public int Seed { get; set; }
-}
